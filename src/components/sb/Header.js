c075c2a92ea43069bfa81539d7287a4b2bfb8219
David Kågell <David.kagell@hotmail.com>
2025-09-04 09:38:27 +0200

    Links in header now working

--- src/components/sb/Header.js
+++ src/components/sb/Header.js
@@ -6,14 +6,15 @@ import { CMS } from "@/utils/cms";
 
 export default async function Header({ blok }) {
   // const headerLinks = await CMS.getProducts();
-console.log("header blok",blok)
+// console.log("header blok",blok)
   return (
     <div
       {...storyblokEditable(blok)} className="w-full bg-fixed min-h-[150px]" style={{backgroundImage: `url(${blok?.logo?.filename})`}}
     >             
 
         {blok.links.map((link, i)=> {
-          return   <div key={i} >{link.label}</div>
+          console.log("link",link)
+          return   <Link href={link.links.cached_url} key={i} >{link.label}</Link>
         })}        
     </div>
   );

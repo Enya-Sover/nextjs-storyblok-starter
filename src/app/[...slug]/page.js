f79810d2720f306361b56e95afdad6c2a0c0c31f
David Kågell <David.kagell@hotmail.com>
2025-09-14 19:54:53 +0200

    fortsätter

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -17,7 +17,7 @@ export default async function Page({ params, searchParams }) {
   return (
     <div className="page p-4">
       <StoryblokStory story={story} />
-      {slug === "products" && <ProductList blok={story.content.body[0]} products={products} />}
+     <ProductList blok={story.content.body[0]} products={products} />
     </div>
   );
 }

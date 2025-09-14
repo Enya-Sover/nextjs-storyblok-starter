e4cd8b651a1c2aec2459883cf89476f9398e9ab7
David Kågell <David.kagell@hotmail.com>
2025-09-14 20:01:05 +0200

    test

--- src/app/[...slug]/page.js
+++ src/app/[...slug]/page.js
@@ -4,23 +4,25 @@ import { CMS } from "@/utils/cms";
 import ProductList from "@/components/sb/ProductList";
 
 export default async function Page({ params, searchParams }) {
-  const slug = await params.slug ? params.slug.join("/") : "home";
+  const slug = params.slug ? params.slug.join("/") : "home";
   const story = await CMS.getStory(slug);
-
-
 
   if (!story) return notFound();
 
   const products = await CMS.getProducts();
   console.log("products in page:", products);
-if(slug=== "products"){
- <ProductList blok={story.content.body[0]} products={products} />
-
-}
 
   return (
     <div className="page p-4">
       <StoryblokStory story={story} />
+      {slug === "products" && (
+        <ProductList
+          blok={story.content.body[0]}
+          products={products}
+          selectedCategoryFromUrl={searchParams?.category || ""}
+        />
+      )}
     </div>
   );
 }
+
